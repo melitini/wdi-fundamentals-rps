f669f28762be6faf6ba89ccb98cf4a5d3e7f7ab0
Melissa M. Dailey <Mel@Ms-MacBook-Pro.local>
2015-08-08 15:32:30 -0700

    FINAL RPS GAME

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -27,25 +27,30 @@ function randomPlay() {
 ////////////////////////////////////////////////
 
 
-//Get move from player
+
+/*==============================================================
+Get move from player
+==============================================================*/
 
 function getPlayerMove(move) {
     move = move || getInput();
     return move;
 }
-
-
-//Get move from computer
+/*==============================================================
+Get move from computer
+==============================================================*/
 
 function getComputerMove(move) {
     move = move || randomPlay(); 
     return move;
 }
+/*==============================================================
+Identify winner 
+==============================================================*/
 
-
-//Identify winner 
+var winner;
 function getWinner(playerMove,computerMove) {
-    var winner;
+
     
     if (playerMove == computerMove) {
          winner = "tie";
@@ -68,62 +73,57 @@ function getWinner(playerMove,computerMove) {
     else if (playerMove == "rock" && computerMove == "paper") {
         winner = "computer";
     }
-    
     return winner;
 }
 
 
-
-
-//Play at least 5 rounds
+/*==============================================================
+Play at least 5 rounds
+==============================================================*/
 
 var playerWins = 0;
 var computerWins = 0;
 
 function playToFive() {
 
-while (computerWins !== 5 && playerWins < 5 || computerWins < 5 && playerWins !== 5) {
-
-    console.log("Let's play Rock, Paper, Scissors");
-
-    var winner = getWinner(playerMove, computerMove);
-    var computerMove = getComputerMove();
-    var playerMove = getPlayerMove();
-
+    while (computerWins < 5 && playerWins < 5) {
     
-    if (playerWins < 5 && computerWins < 5) {
+        console.log("Let's play Rock, Paper, Scissors");
     
-        if (winner === "player") {
-            playerWins += 1;
-            computerWins += 0;
-        } 
-        else if (winner === "computer") {
-            computerWins += 1;
-            playerWins += 0;
-        } 
-        else {
-            playerWins += 0;
-            computerWins +=0;
+        var computerMove = getComputerMove();
+        var playerMove = getPlayerMove();
+        var winner = getWinner(playerMove, computerMove);
+        
+        if (playerWins < 5 && computerWins < 5) {
+        
+            if (winner === "player") {
+                playerWins += 1;
+                computerWins += 0;
+                console.log("Player wins!");
+            } 
+            else if (winner === "computer") {
+                computerWins += 1;
+                playerWins += 0;
+                console.log("Oh no! Computer beats player");
+            } 
+            else if (winner === "tie"){
+                playerWins += 0;
+                computerWins +=0;
+                console.log("It's a tie!");
+            }
+            else {
+                console.log("WTf was that? Get it together and try again");
+            }
+    console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
+        
+    console.log('The score is currently ' + playerWins + ' to ' + computerWins);
         }
-console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
-    
-console.log('The score is currently ' + playerWins + ' to ' + computerWins);
-    
-
-
-    }
-
-
-
-
-  }
-
+      }
 return [playerWins, computerWins];
-
 }
-
 
 //Call it! 
 playToFive();
 
  
+
